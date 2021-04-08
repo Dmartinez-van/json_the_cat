8207a89aca7aa4ce94172d9a85cfa4a72af0e406
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-08 09:22:24 -0700

    Fixed, working

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,20 +4,20 @@ const args = process.argv.slice(2);
 
 const breed = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error === null) {
-    console.log(`oops,  "${breed}"  is not a valid breed. Try again. `);
-  }
+request(`https://api.thecatdapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   if (error) {
     console.log("Seems like you encountered an error... hmm, here are the detaisl: ");
-    //throw error;
+    console.log("Tried to access the site: ", error.hostname);
     throw error;
   }
   
   const data = JSON.parse(body);
+  
+  if (data[0] === undefined) {
+    console.log(`oops,  "${breed}"  is not a valid breed. Try again. `);
+    process.exit();
+  }
 
-  // console.log("body: ", data);
-  console.log("typeof body", typeof data);
-  console.log(data[0]);
+  console.log(data[0].description);
   process.exit();
 });
